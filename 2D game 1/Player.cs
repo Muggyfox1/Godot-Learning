6e25a881aec45a7a7b3ee4d64d3bf113d9ec98cb
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-04-09 23:18:22 -0400

    more progress on tutorial
    interesting so far. Unity is definitely easier/faster to use. But maybe it gets better?

--- 2D game 1/Player.cs
+++ 2D game 1/Player.cs
@@ -4,49 +4,72 @@ using System;
 public class Player : Area2D
 {
 	[Export]
-	public int Speed = 400; // How fast the player will move (pixels/sec).
-
-	public Vector2 ScreenSize; // Size of the game window.
+	public int Speed = 400;
+	public Vector2 ScreenSize;
 	
-	public override void _Ready()
+	[Signal]
+	public delegate void Hit();
+	
+	public void Start(Vector2 pos)
 	{
-		ScreenSize = GetViewportRect().Size;
+		Position = pos;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
 	}
 	
-	public override void _Process(float delta)
-	{
-		var velocity = Vector2.Zero; // The player's movement vector.
-
-		if (Input.IsActionPressed("move_right"))
-		{
+	public override void _Ready(){
+		ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
+	
+	public override void _Process(float delta){
+		var velocity = Vector2.Zero;
+		var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+		
+		if(Input.IsActionPressed("move_right")){
 			velocity.x += 1;
 		}
-
-		if (Input.IsActionPressed("move_left"))
-		{
+		
+		if(Input.IsActionPressed("move_left")){
 			velocity.x -= 1;
 		}
-
-		if (Input.IsActionPressed("move_down"))
-		{
+		
+		if(Input.IsActionPressed("move_down")){
 			velocity.y += 1;
 		}
-
-		if (Input.IsActionPressed("move_up"))
-		{
+		
+		if(Input.IsActionPressed("move_up")){
 			velocity.y -= 1;
 		}
-
-		var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
-
-		if (velocity.Length() > 0)
-		{
+		
+		if(velocity.Length() > 0){
 			velocity = velocity.Normalized() * Speed;
 			animatedSprite.Play();
-		}
-		else
-		{
+		}else{
 			animatedSprite.Stop();
 		}
+		
+		Position += velocity * delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
+			y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
+		);
+		
+		if(velocity.x != 0){
+			animatedSprite.Animation = "walk";
+			animatedSprite.FlipV = false;
+			animatedSprite.FlipH = velocity.x < 0;
+			
+		} else if(velocity.y != 0){
+			animatedSprite.Animation = "up";
+			animatedSprite.FlipV = velocity.y > 0;
+		}
+	}
+	
+	private void _on_Player_body_entered(object body)
+	{
+		Hide();
+		EmitSignal(nameof(Hit));
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
 	}
 }
